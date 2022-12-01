50e149cc432a0da303ef682c82c7c78611e805f9
paul <paulkelechi11@gmail.com>
2022-11-30 16:49:51 -0800

    Created a function that checks if there is a cycle in a list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,37 +1,34 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 
 /**
- * check_cycle - checks if a cycle exists in a linked list
- * @list: list to be checked
- * Return: 0 is no cycle; 1 if cycle
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: Linked list to be checked
+ *
+ * Return: If there is no cycle return -0.
+ *         If there is a cycle return -1
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *current;
-	listint_t *head;
-	listint_t *temp;
-	__attribute__((unused)) int i, compare_i;
+  listint_t *turtle, *hare;
 
-	i = 0;
-	current = list;
-	head = list;
-	while (current != NULL)
-	{
-		compare_i = 0;
-		temp = head;
-		while (compare_i < i)
-		{
-			if (current == temp)
-				return (1);
-			compare_i++;
-			temp = temp->next;
-		}
-		current = current->next;
-		i++;
-	}
-	return (0);
+  if (list == NULL || list->next == NULL)
+    return (0);
+
+  turtle = list->next;
+  hare = list->next->next;
+
+  while (turtle && hare && hare->next)
+    {
+      if (turtle == hare)
+	return (1);
+
+      turtle = turtle->next;
+      hare = hare->next->next;
+    }
+
+  return (0);
 }
+
