2c3e39cd942fe13c67971171791059aff309fc58
paul <paulkelechi11@gmail.com>
2022-12-06 16:38:50 -0800

    Created a function that retrieves an item at an index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-
 def element_at(my_list, idx):
-    """Retrive an element from a list."""
-    if idx < 0 or idx > (len(my_list) - 1):
-        return None
-    return (my_list[idx])
+    if idx < 0:
+        return none
+    length = len(my_list)
+    if idx > length:
+        return none
+    return my_list[idx]
