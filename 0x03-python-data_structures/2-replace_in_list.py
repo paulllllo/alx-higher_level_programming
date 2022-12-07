add5356a9ed609b3b0932ccc77076a005029d8e5
paul <paulkelechi11@gmail.com>
2022-12-06 17:05:05 -0800

    [Corrections]

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,10 @@
 
 def replace_in_list(my_list, idx, element):
     length = len(my_list)
+    new_list = my_list
     if idx < 0:
         return my_list
     if idx > length:
         return my_list
-    my_list[idx] = element
-    return my_list
+    new_list[idx] = element
+    return new_list
