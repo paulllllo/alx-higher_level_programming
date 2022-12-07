b141949233b5c9ae664a47c1fd91d28e7b8aa52b
paul <paulkelechi11@gmail.com>
2022-12-06 17:01:32 -0800

    Created a fuunction that replaces an item in a list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,6 +5,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
     if idx > length:
-        return none
+        return my_list
     my_list[idx] = element
     return my_list
