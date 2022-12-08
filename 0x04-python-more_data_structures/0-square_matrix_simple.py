343d11eb92d997d79de88d0c86ce4089ea989b77
paul <paulkelechi11@gmail.com>
2022-12-08 05:25:21 -0800

    A function that squares all the integers in a square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,8 @@
 
 """Function to square all the integers in a matrix"""
 
+
 def square_matrix_simple(matrix=[]):
+
     new_matrix = list(map(lambda item: [i**2 for i in item], matrix))
     return new_matrix
