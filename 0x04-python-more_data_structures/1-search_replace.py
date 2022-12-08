6770acf1493295175a4de170d3cb8806d8951c76
paul <paulkelechi11@gmail.com>
2022-12-08 05:58:21 -0800

    Created a function that replaces an element in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,5 +5,6 @@
 
 def search_replace(my_list, search, replace):
 
-    new_list = list(map(lambda item: replace if item == search else item, my_list))
+    new_list = list(map(lambda item: replace if item == search
+                        else item, my_list))
     return new_list
