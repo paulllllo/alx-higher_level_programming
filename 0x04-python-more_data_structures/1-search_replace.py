d7ae71cbe297f33f2d7557e04db586429a22e244
paul <paulkelechi11@gmail.com>
2022-12-08 05:56:30 -0800

    Created a function that replaces an element in a list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Function that replaces an element with another in a list"""
+
+
+def search_replace(my_list, search, replace):
+
+    new_list = list(map(lambda item: replace if item == search else item, my_list))
+    return new_list
