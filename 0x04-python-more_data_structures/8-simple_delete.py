4cbc2a236f90d5780e35b7581bd2c320d4729587
paul <paulkelechi11@gmail.com>
2022-12-08 19:34:59 -0800

    Created a function that removes an element from a dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -5,7 +5,7 @@
 
 def simple_delete(a_dictionary, key=""):
 
-    if not key in a_dictionary:
+    if key not in a_dictionary:
         return a_dictionary
     del a_dictionary[key]
     return a_dictionary
