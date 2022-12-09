129697b9ac96f36fc7e22a30809c68eb2fd26974
paul <paulkelechi11@gmail.com>
2022-12-08 20:26:39 -0800

    Created a function that multiplies elements in a list by a number

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Function that multiplies values in a list by a number"""
+
+
+def multiply_list_map(my_list=[], number=0):
+
+    return list(map(lambda x: x * number, my_list))
