00ec84d6927b49738f8a545cf6d6dcff1c33e94f
paul <paulkelechi11@gmail.com>
2022-12-08 18:38:02 -0800

    Created a function that returns the number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Function that returns the number of keys in a dictionary"""
+
+
+def number_keys(a_dictionary):
+
+    num = 0
+    for i in a_dictionary:
+        num = num + 1
+    return num
---
+++ 0x04-python-more_data_structures/5-number_keys.py~
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Function that returns a set of different elements in rtwo sets"""
+
+
+def only_diff_elements(set_1, set_2):
+
+    return set_1 ^ set_2
