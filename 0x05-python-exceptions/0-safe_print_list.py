1f4c9c097a5583b473437e5ffe5e3290381f8c18
paul <paulkelechi11@gmail.com>
2022-12-20 16:35:35 -0800

    Created a function that demos a ValueError exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[i]}", end="")
             count = count + 1
         except IndexError:
-            pass
+            None
     print("")
     return count
