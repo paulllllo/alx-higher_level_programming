d92dcde4059633ca34e0d05d9bd898ad56205fcc
paul <paulkelechi11@gmail.com>
2023-01-11 15:39:21 -0800

    [Correction]

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,6 +6,14 @@ class Rectangle:
     """A class that contains some attributes and methods/
     around a rectange"""
     def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
         self.__width = width
 
