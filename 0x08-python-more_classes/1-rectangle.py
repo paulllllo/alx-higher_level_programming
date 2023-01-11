5a6c9d1fcb7e061a25545f9d728053da3f49fbb6
paul <paulkelechi11@gmail.com>
2023-01-11 07:20:43 -0800

    Created a class that defines attributes and properties of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """A class that contains some attributes and methods/
     around a rectange"""
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
