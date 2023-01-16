8a04d2a65ac99f6603673d5214439bebc9d2075f
paul <paulkelechi11@gmail.com>
2023-01-16 06:36:29 -0800

    Created a method that indicates when an object is deleted

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """A class that contains some attributes and methods/
+    around a rectange"""
+    def __init__(self, width=0, height=0):
+        """A rectangle with width and height attributes"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """Gets the width of the rectangle"""
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle"""
+        if not (isinstance(value, int)):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height of the rectangle"""
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle"""
+        if not (isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates the area of the rectangle"""
+        return(self.__width * self.__height)
+
+    def perimeter(self):
+        """calculates the perimeter of the rectangle"""
+        if ((self.__width == 0) | (self.__height == 0)):
+            return(0)
+        return((2 * self.__width) + (2 * self.__height))
+
+    def __str__(self):
+        """A string reprisentaion of object/instance with 'H'"""
+        if ((self.__width == 0) | (self.__height == 0)):
+            return("")
+        line = "#" * self.__width
+        return("\n".join([line] * self.__height))
+
+    def __repr__(self):
+        """A formal string representation of object/instance"""
+        return f'Rectangle({self.__width}, {self.__height})'
+
+    def __del__(self):
+        """A method that is called when all instances of an object is deleted"""
+        print("Bye rectangle...")
