4600ecc31ae5cb1afda67a7f2e38d4712ecc2c73
paul <paulkelechi11@gmail.com>
2023-03-24 04:20:39 -0700

    Created a script that prints the first object from a SQLAlchemy query

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A script that lists all States"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   3306, sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).order_by(State.id.asc()).first()
+    if instance:
+        print(f"{instance.id}: {instance.name}")
+    else:
+        print("")
