a81228513a037bcbc7bf5d954eee8ee050ed949e
paul <paulkelechi11@gmail.com>
2023-03-24 12:22:30 -0700

    Created a script that inserts a row in a table with SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that inserts a row into a table"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:{}/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   3306, sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    newState = State(name="Louisiana")
+    session.add(newState)
+    session.commit()
+    print(newState.id)
