bcb5958f69dde29a5fd7e0cedcdac377c2aeff21
Kelechi <paulkelechi11@gmail.com>
2023-05-26 06:01:49 -0700

    [COrrection]

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const request = require("request")
-const movieId = process.argv[2]
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
-    if (error){
-	return console.error(error);
-    }
-    console.log(JSON.parse(body).title)
+  if (error) {
+    return console.error(error);
+  }
+  console.log(JSON.parse(body).title);
 });
