fa9d182a99acad1f8762b0b32016e1213fb907fb
Paul <paulkelechi11@gmail.com>
2023-07-04 21:38:54 +0100

    removed required dirs

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const { readFile } = require('fs');
-readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(data);
-});
--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-request(process.argv[2], (err, response) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log('code:', response && response.statusCode);
-});
