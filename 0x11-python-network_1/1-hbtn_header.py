7ab87cbdbe05949990056cfaf4b9c56e08197ded
paul <paulkelechi11@gmail.com>
2023-07-06 03:38:09 -0700

    added script that requests cmd line arg and prints header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A script that prints the response from a request from cmd arguement"""
+
+import urllib.request
+import sys
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    the_header = response.info()
+
+print(the_header['X-Request-Id'])
