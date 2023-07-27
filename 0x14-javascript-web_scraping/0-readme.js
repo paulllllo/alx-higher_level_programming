22ab76101118a0c45f0d0116ed9689ab5b523dfd
paul <paulkelechi11@gmail.com>
2023-07-26 19:39:01 -0700

    Corrections

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 // A script created to print the content of a file
 
-let fs = require('fs');
+const fs = require('fs');
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log(data);
-    }
-})
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
