18008117b6e6df9ad9a8c35d6921e483b3f2ecf6
paul <paulkelechi11@gmail.com>
2023-08-02 18:20:13 -0700

    Rectified script issues

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+$(function () {
+	const hello = $('#hello');
+	$.ajax({
+		type: 'GET',
+		url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+		success: (data) => {
+			hello.text(data.hello);	
+		}
+	});
+});
